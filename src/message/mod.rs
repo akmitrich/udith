b4ebe8c6238afea53999b41749de16ee8a786e4d
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2024-04-19 11:51:01 +0300

    refactor: To generic message type

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,40 +1,17 @@
+mod generic;
 mod method;
 mod raw;
 pub mod start_line;
 mod status_code;
 
-use std::ops::Deref;
-
-use bstr::ByteSlice;
+pub use generic::*;
 pub use method::*;
 pub use raw::*;
+pub use start_line::StartLine;
 pub use status_code::*;
 
 const CRLF: &[u8] = b"\r\n";
-
-pub struct Request<'this> {
-    request_line: start_line::RequestLine<'this>,
-}
-
-impl<'a> Request<'a> {
-    pub fn method(&self) -> Method {
-        self.request_line.method
-    }
-
-    pub fn request_uri(&self) -> &[u8] {
-        self.request_line.uri
-    }
-}
-
-impl<'a> TryFrom<&'a Raw> for Request<'a> {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &'a Raw) -> Result<Self, Self::Error> {
-        let mut lines = value.deref().split_str(CRLF);
-        let request_line = start_line::try_to_take_request_line(&mut lines)?;
-        Ok(Self { request_line })
-    }
-}
+const SIP_VERSION: &[u8] = b"SIP/2.0";
 
 #[cfg(test)]
 mod tests {
@@ -44,8 +21,12 @@ mod tests {
     fn create_request_message() {
         let data = b"INVITE sip:127.0.0.1:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 127.0.0.1:40675;rport;branch=z9hG4bK7rmHHX13H1N3e\r\nMax-Forwards: 50\r\nFrom: <sip:127.0.0.1:40675>;tag=7m5yaggg50pKc\r\nTo: <sip:127.0.0.1:5060>\r\nCall-ID: b4e3ef6e-7802-123d-568f-c01803268e70\r\nCSeq: 980604667 INVITE\r\nContact: <sip:127.0.0.1:40675;transport=udp>\r\nUser-Agent: Udith Client 0.0.0\r\nAllow: INVITE, ACK, BYE, CANCEL, OPTIONS, PRACK, MESSAGE, SUBSCRIBE, NOTIFY, REFER, UPDATE\r\nSupported: timer, 100rel\r\nContent-Type: application/sdp\r\nContent-Disposition: session\r\nContent-Length: 398\r\n\r\nv=0\r\no=UniMRCPClient 5074391966795348619 3411008761561041293 IN IP4 192.168.50.157\r\ns=-\r\nc=IN IP4 127.0.1.1\r\nt=0 0\r\nm=application 9 TCP/MRCPv2 1\r\na=setup:active\r\na=connection:new\r\na=resource:speechrecog\r\na=cmid:1\r\nm=audio 4000 RTP/AVP 0 8 96 101\r\na=rtpmap:0 PCMU/8000\r\na=rtpmap:8 PCMA/8000\r\na=rtpmap:96 L16/8000\r\na=rtpmap:101 telephone-event/8000\r\na=fmtp:101 0-15\r\na=sendonly\r\na=ptime:20\r\na=mid:1\r\n".to_vec();
         let raw = Raw::new(data);
-        let req = Request::try_from(&raw).unwrap();
-        assert_eq!(Method::Invite, req.method());
-        assert_eq!(b"sip:127.0.0.1:5060", req.request_uri());
+        let req = Message::try_from(&raw).unwrap();
+        assert!(req.start_line.is_request());
+        let StartLine::Request(request_line) = &req.start_line else {
+            unreachable!()
+        };
+        assert_eq!(Method::Invite, request_line.method);
+        assert_eq!(b"sip:127.0.0.1:5060", request_line.uri);
     }
 }
--- src/message/start_line.rs
+++ src/message/start_line.rs
@@ -1,6 +1,29 @@
+use super::{try_to_take_method, try_to_take_status_code, Method, StatusCode, CRLF, SIP_VERSION};
+use anyhow::Ok;
 use bstr::ByteSlice;
 
-use super::{try_to_take_method, try_to_take_status_code, Method, StatusCode, CRLF};
+pub enum StartLine<'raw> {
+    Request(RequestLine<'raw>),
+    Status(StatusLine<'raw>),
+}
+
+impl<'a> StartLine<'a> {
+    pub fn parse(raw: &'a [u8]) -> Result<Self, anyhow::Error> {
+        if raw.starts_with(SIP_VERSION) {
+            Ok(Self::Status(StatusLine::parse(raw)?))
+        } else {
+            Ok(Self::Request(RequestLine::parse(raw)?))
+        }
+    }
+
+    pub fn is_request(&self) -> bool {
+        matches!(self, Self::Request(_))
+    }
+
+    pub fn is_status(&self) -> bool {
+        matches!(self, Self::Status(_))
+    }
+}
 
 pub struct RequestLine<'raw> {
     pub method: Method,
@@ -25,7 +48,7 @@ impl<'a> RequestLine<'a> {
         let version = splitted
             .next()
             .ok_or_else(|| anyhow::Error::msg("No version found in request-line"))?;
-        if version != b"SIP/2.0" {
+        if version != SIP_VERSION {
             return Err(anyhow::Error::msg(format!(
                 "Invalid SIP version in request-line: {:?}",
                 version
@@ -44,7 +67,7 @@ impl<'a> StatusLine<'a> {
         let version = splitted
             .next()
             .ok_or_else(|| anyhow::Error::msg("No version found in status-line"))?;
-        if version != b"SIP/2.0" {
+        if version != SIP_VERSION {
             return Err(anyhow::Error::msg(format!(
                 "Invalid SIP version in status-line: {:?}",
                 version
@@ -59,13 +82,13 @@ impl<'a> StatusLine<'a> {
     }
 }
 
-pub(super) fn try_to_take_request_line<'a>(
+pub(super) fn try_to_take_start_line<'a>(
     lines: &mut bstr::Split<'a, 'a>,
-) -> Result<RequestLine<'a>, anyhow::Error> {
+) -> Result<StartLine<'a>, anyhow::Error> {
     let line = lines
         .next()
         .ok_or_else(|| anyhow::Error::msg("No request-line found"))?;
-    RequestLine::parse(line)
+    StartLine::parse(line)
 }
 
 #[cfg(test)]
@@ -88,5 +111,34 @@ mod tests {
         assert!(RequestLine::parse(b"INVITE sip:127.0.0.1:5060 ").is_err());
         assert!(RequestLine::parse(b"INVITE sip:127.0.0.1:5060").is_err());
         assert!(RequestLine::parse(b"sip:127.0.0.1:5060").is_err());
+    }
+
+    #[test]
+    fn status_line_works() {
+        let line = b"SIP/2.0 200 OK";
+        let status_line = StatusLine::parse(line).unwrap();
+        assert_eq!(200_u16, status_line.status_code.into());
+        assert_eq!(b"OK", status_line.reason_phrase);
+    }
+
+    #[test]
+    fn status_line_must_fail() {
+        assert!(StatusLine::parse(b"").is_err());
+        assert!(StatusLine::parse(b"SIP/2.0 200 OK\r\n").is_err());
+        assert!(StatusLine::parse(b"SIP/1.1 200 OK").is_err());
+        assert!(StatusLine::parse(b"SIP/2.0  OK").is_err());
+        assert!(StatusLine::parse(b"SIP/2.0 OK").is_err());
+    }
+
+    #[test]
+    fn start_line_works() {
+        let from_request = b"INVITE sip:127.0.0.1:5060 SIP/2.0";
+        let start_line = StartLine::parse(from_request).unwrap();
+        assert!(start_line.is_request());
+        let from_response = b"SIP/2.0 200 OK";
+        let start_line = StartLine::parse(from_response).unwrap();
+        assert!(start_line.is_status());
+        let failed = StartLine::parse(b"");
+        assert!(failed.is_err());
     }
 }
